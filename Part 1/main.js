f1a262fa644e678d59f2d17514bd087244cb3d5f
Mr. Long <josephminhlong@gmail.com>
2024-03-02 11:23:45 +0700

    first commit

---
+++ Part 1/main.js
@@ -0,0 +1,31 @@
+/**
+ *  Student: Nguyễn Vũ Minh Long
+ *  Course: JavaScript Full Course - Beginner to Pro - Part 1
+ *  Author: SuperSimpleDev
+ *  Link video: https://www.youtube.com/watch?v=SBmSRK3feww&list=PLEPye7A7EcQZrT3VSBb7jtxnxIfY3yyG6&index=4&        ab_channel=SuperSimpleDev
+ */
+
+/* 
+    Order of operation (Operator precedence): Độ ưu tiên của toán tử
+    1. ( ... )
+    2. * /
+    3. We can use brackets to control which part of caculation gets
+*/
+
+console.log(3 + 4 * 5); // 3 + 20
+// Expected output: 23
+
+console.log(4 * 3 ** 2); // 4 * 9 (nhân 3 2 lần = 9)
+// Expected output: 36
+
+console.log(3 ** 3); // 27 (3 * 3 *3)
+
+let a;
+let b;
+
+console.log((a = b = 5));
+// Expected output: 5
+
+/*
+
+*/
