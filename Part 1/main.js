fa48811c981364fb9c2d558ce97f917d58d38122
longminhdev <longminhdev@gmail.com>
2024-03-02 12:49:00 +0700

    first commit

--- Part 1/main.js
+++ Part 1/main.js
@@ -1,15 +1,35 @@
-/**
- *  Student: Nguyễn Vũ Minh Long
- *  Course: JavaScript Full Course - Beginner to Pro - Part 1
- *  Author: SuperSimpleDev
- *  Link video: https://www.youtube.com/watch?v=SBmSRK3feww&list=PLEPye7A7EcQZrT3VSBb7jtxnxIfY3yyG6&index=4&        ab_channel=SuperSimpleDev
- */
-
 /* 
-    Order of operation (Operator precedence): Độ ưu tiên của toán tử
-    1. ( ... )
-    2. * /
-    3. We can use brackets to control which part of caculation gets
+    I/ Order of operation (Operator precedence): Độ ưu tiên của toán tử
+        1. ( ... )
+        2. * /
+        3. We can use brackets to control which part of caculation gets
+
+    II/ Weird behavior of math in Javascript
+        In programming:
+            2, 3, 4 = integers
+            2.8, 2.9 = floating point numbers(floats)
+
+            *   Computer have problem working with floats.
+                    Ex: 
+                        0.1 + 0.2 = 0.300000000004 (werid number)
+                        20.95 + 7.99 = 28.9399999998999
+
+            *   How do we avoid this problem?
+                Best practice when calculating money = calculate in cents instead of dollars.
+                    Ex: 
+                        (2095 + 799) / 100 = 28.94 (converted to dollars)
+
+        - Calcualating with floats are sometimes inaccurate.
+        - When working with money 
+            1. Do the calculation in cents
+            2. Conver to dollars
+
+    III/ How to round the number
+        Ex:
+            - Math.round() = NaN (Not a number)
+
+    
+
 */
 
 console.log(3 + 4 * 5); // 3 + 20
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 * Course: JavaScript Full Course - Beginner to Pro
 * Author: SuperSimpleDev
 * Link youtube: [JavaScript Full Course - Beginner to Pro](https://www.youtube.com/watch?v=G3e-cpL7ofc&list=PLEPye7A7EcQZrT3VSBb7jtxnxIfY3yyG6&ab_channel=SuperSimpleDev)
+* Practive Interview Javascript [Practice Interview Javascript](https://www-hellojavascript-info.translate.goog/docs/general-javascript-questions/javascript-fundamentals/basic-math-operators-in-javascript?_x_tr_sl=en&_x_tr_tl=vi&_x_tr_hl=vi&_x_tr_pto=sc)
